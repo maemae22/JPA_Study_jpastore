6288abe511a9772e14548a2a58c12cef42eb27d9
maemae22 <maemae2@naver.com>
2023-04-08 03:11:22 +0900

    Order Service 개발 (주문 서비스 개발)

---
+++ src/main/java/jpabook/jpastore/service/OrderService.java
@@ -0,0 +1,62 @@
+package jpabook.jpastore.service;
+
+import jpabook.jpastore.domain.Delivery;
+import jpabook.jpastore.domain.Member;
+import jpabook.jpastore.domain.Order;
+import jpabook.jpastore.domain.OrderItem;
+import jpabook.jpastore.domain.item.Item;
+import jpabook.jpastore.repository.ItemRepository;
+import jpabook.jpastore.repository.MemberRepository;
+import jpabook.jpastore.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    // 주문
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // Entity 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        // CascadeType.ALL 으로 설정되어 있기 때문에, OrderItem이랑 Delivery도 같이 persist 됨
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    // 주문 취소
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    // 검색
+}
