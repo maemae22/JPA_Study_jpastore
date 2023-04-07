72c91adec778da8e7b2f59d26cac9171f44aa4b2
maemae22 <maemae2@naver.com>
2023-04-07 21:42:23 +0900

    JPA와 DB 설정, 동작확인을 위해 만들었던 파일들 주석 처리

--- src/main/java/jpabook/jpastore/Member.java
+++ src/main/java/jpabook/jpastore/Member.java
@@ -1,19 +1,19 @@
-package jpabook.jpastore;
-
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
-@Getter @Setter
-public class Member {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-
-    private String username;
-}
+//package jpabook.jpastore;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//
+//@Entity
+//@Getter @Setter
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    private String username;
+//}
