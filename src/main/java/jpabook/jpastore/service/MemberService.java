a61f8d07adf83340450c4f267ef9e95cc40437e9
maemae22 <maemae2@naver.com>
2023-04-07 23:37:51 +0900

    Member Service 개발 (회원 서비스 개발)

---
+++ src/main/java/jpabook/jpastore/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpastore.service;
+
+import jpabook.jpastore.domain.Member;
+import jpabook.jpastore.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 같은 이름을 가진 회원이 있는지 검증 (중복 회원 검증)
+    private void validateDuplicateMember(Member member) {
+        // EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
