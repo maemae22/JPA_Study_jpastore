c70f8901d100c10fe3f30c2d5486f95be442dc5c
maemae22 <maemae2@naver.com>
2023-04-08 22:57:22 +0900

    상품 수정 시 병합(merge)이 아닌 변경 감지(dirty checking)로 데이터 수정이 일어나도록 코드 리팩토링

--- src/main/java/jpabook/jpastore/controller/ItemController.java
+++ src/main/java/jpabook/jpastore/controller/ItemController.java
@@ -63,7 +63,8 @@ public class ItemController {
     }
 
     @PostMapping("/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute("form") BookForm form) {
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
+        /*
         Book book = new Book();
         book.setId(form.getId());
         book.setName(form.getName());
@@ -73,6 +74,9 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
+         */
+
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
