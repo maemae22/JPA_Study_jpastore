198810307c172e4bec70c2028c3a96230c1a38ef
maemae22 <maemae2@naver.com>
2023-04-10 01:49:12 +0900

    주문 조회 V3.1 : 엔티티를 DTO로 변환 - 페이징과 한계 돌파
    - 지연 로딩 성능 최적화를 위해 hibernate.default_batch_fetch_size, @BatchSize 를 적용 (이 옵션을 사용하면 컬렉션이나, 프록시 객체를 한꺼번에 설정한 size 만큼 IN 쿼리로 조회)
    - 컬렉션 페치 조인은 페이징이 불가능 하지만 이 방법은 페이징이 가능

--- src/main/java/jpabook/jpastore/domain/item/Item.java
+++ src/main/java/jpabook/jpastore/domain/item/Item.java
@@ -4,11 +4,13 @@ import jpabook.jpastore.domain.Category;
 import jpabook.jpastore.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+//@BatchSize(size = 100)
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
