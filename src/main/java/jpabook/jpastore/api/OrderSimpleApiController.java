702d4f928ad7bde37b91ef8c21e00de0749e02d4
maemae22 <maemae2@naver.com>
2023-04-09 18:50:24 +0900

    간단한 주문 조회 V2 : 엔티티를 DTO로 변환 (fetch join 사용X)
    단점: 지연로딩으로 쿼리 N번 호출

--- src/main/java/jpabook/jpastore/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpastore/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpastore.api;
 
+import jpabook.jpastore.domain.Address;
 import jpabook.jpastore.domain.Order;
+import jpabook.jpastore.domain.OrderStatus;
 import jpabook.jpastore.repository.OrderRepository;
 import jpabook.jpastore.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 [ xToOne 성능 최적화하기 (ManyToOne, OneToOne) ]
@@ -37,5 +42,37 @@ public class OrderSimpleApiController {
         }
 
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환 (fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDTO> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDTO> result = orders.stream()
+                .map(order -> new SimpleOrderDTO(order))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        // DTO가 Entity를 파라미터로 받는 것은 괜찮다 !
+        public SimpleOrderDTO(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // Lazy 초기화
+        }
     }
 }
